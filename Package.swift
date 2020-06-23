6998f45f75b24f4d5e979de2be20295339cea738
Stanislav Shemiakov <stas2992@gmail.com>
2020-06-23 18:22:35 +0300

    fixed spm manifest

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftCssParser",
-            dependencies: []),
+            dependencies: [],
+            path: "SwiftCssParser"),
         .testTarget(
             name: "SwiftCssParserTests",
-            dependencies: ["SwiftCssParser"]),
+            dependencies: ["SwiftCssParser"],
+            path: "SwiftCssParserTests"),
     ]
 )
